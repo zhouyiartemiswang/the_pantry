2a726bd815e99875b150892842feb38fbc2a7228
Artemis <wzyzhouyi@gmail.com>
2020-11-14 16:48:44 -0800

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 import Login from './pages/Login';
+import Copyright from './components/Copyright';
+import Box from '@material-ui/core/Box';
 
 function App() {
     return (
         <BrowserRouter>
             <NavBar />
             <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
                 <Route exact path="/login">
                     <Login />
                 </Route>
             </Switch>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
         </BrowserRouter>
     );
 }
