0079d1fdfa405ce43707c6a6f8fee596241569ec
Artemis <wzyzhouyi@gmail.com>
2020-11-15 13:58:32 -0800

    added inventory alert

--- src/components/InventoryAlert/index.js
+++ src/components/InventoryAlert/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import './style.css';
 
 export default function InventoryAlert() {
     return (
-        <div>
-        </div>
+        <>
+            <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                Alerts
+            </Typography>
+            <Typography component="p" variant="h9" color="secondary" gutterBottom>
+                Whole Milk is low in stock.
+            </Typography>
+        </>
     )
 }
