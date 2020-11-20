4ce7bf8509baf2fc9020686bcf467f37890bf7ef
Artemis <wzyzhouyi@gmail.com>
2020-11-19 22:19:23 -0800

    fixed premade cake page

--- src/components/PreMadeCakeDialog/index.js
+++ src/components/PreMadeCakeDialog/index.js
@@ -6,7 +6,7 @@ import './style.css';
 const useStyles = makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1),
-        minWidth: 120,
+        // minWidth: 120,
     },
 }));
 
@@ -17,8 +17,8 @@ export default function PreMadeCakeDialog() {
     const [itemState, setItemState] = useState({
         name: "",
         image: "",
-        // size: "",
         price: "",
+        image: "",
         ingredients: "",
         description: "",
     });
@@ -32,6 +32,8 @@ export default function PreMadeCakeDialog() {
     };
 
     const handleInputChange = (event) => {
+        // console.log(event.target.name);
+        // console.log(event.target.value);
         const { name, value } = event.target;
         setItemState({
             ...itemState,
@@ -41,19 +43,31 @@ export default function PreMadeCakeDialog() {
 
     const handleInputSubmit = (event) => {
         event.preventDefault();
+        // const token = localStorage.getItem("token");
+        // console.log(itemState);
         handleClose();
-        const token = localStorage.getItem("token");
     }
 
     return (
         <div>
-            <Button variant="outlined" color="primary" onClick={handleOpen}>Add Pre-Made Cakes</Button>
+            <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleOpen}
+            >
+                Add Pre-Made Cakes
+            </Button>
+
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+
                 <DialogTitle id="form-dialog-title">Add a pre-made cake to your cake master page</DialogTitle>
+
                 <DialogContent>
+
                     <FormControl className={classes.formControl}>
                         <TextField
-                            required autoFocus
+                            required
+                            multiline={true}
                             id="name"
                             label="Name"
                             value={itemState.name}
@@ -61,20 +75,11 @@ export default function PreMadeCakeDialog() {
                             onChange={handleInputChange}
                         />
                     </FormControl>
+
                     <FormControl className={classes.formControl}>
                         <TextField
-                            required autoFocus
-                            type="number"
-                            id="size"
-                            label="Size (in)"
-                            value={itemState.size}
-                            name="size"
-                            onChange={handleInputChange}
-                        />
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <TextField
-                            required autoFocus
+                            required
+                            multiline={true}
                             type="number"
                             id="price"
                             label="Price&nbsp;($)"
@@ -83,9 +88,11 @@ export default function PreMadeCakeDialog() {
                             onChange={handleInputChange}
                         />
                     </FormControl>
+
                     <FormControl className={classes.formControl}>
                         <TextField
-                            required autoFocus
+                            required
+                            multiline={true}
                             id="ingredients"
                             label="Ingredients"
                             value={itemState.ingredients}
@@ -93,9 +100,11 @@ export default function PreMadeCakeDialog() {
                             onChange={handleInputChange}
                         />
                     </FormControl>
+
                     <FormControl className={classes.formControl}>
                         <TextField
-                            required autoFocus
+                            required
+                            multiline={true}
                             id="description"
                             label="Description"
                             value={itemState.description}
@@ -103,12 +112,25 @@ export default function PreMadeCakeDialog() {
                             onChange={handleInputChange}
                         />
                     </FormControl>
+
+                    <FormControl className={classes.formControl}>
+                        <TextField
+                            required
+                            multiline={true}
+                            id="image"
+                            label="Image URL"
+                            value={itemState.image}
+                            name="image"
+                            onChange={handleInputChange}
+                        />
+                    </FormControl>
+
                 </DialogContent>
 
                 <DialogActions>
-                    <Button color="primary">
+                    {/* <Button color="primary">
                         Upload Image
-                    </Button>
+                    </Button> */}
                     <Button onClick={handleClose} color="primary">
                         Cancel
                     </Button>
