9e4702f0c51f659cf72d9ee24448e6ccd2c5e858
Artemis <wzyzhouyi@gmail.com>
2020-11-22 15:41:39 -0800

    added homepage styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Toolbar } from '@material-ui/core';
+import { Link, Button } from '@material-ui/core';
 import './style.css';
 
 export default function Home() {
     return (
         <>
             <div className="cover-img">
-                <Toolbar />
             </div>
             <div className="tagline">
-                <h1 style={{ textAlign: "center" }}>THE PANTRY</h1>
+                <h1>THE PANTRY</h1>
                 <p>A platform for small-scale bakery owners to promote and manage their businesses.</p>
+                <Link href="/cakemasters">
+                    <Button variant="outlined">Explore More</Button>
+                </Link>
             </div>
         </>
     )
